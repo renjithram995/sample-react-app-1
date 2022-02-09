cfa80e30cff19181f416c2270e3f8f3263b71c44
Renjith Ramaswamy <rrenjith1995@gmail.com>
2022-02-09 15:10:46 +0530

    portals

--- src/components/UI/Error.js
+++ src/components/UI/Error.js
@@ -1,22 +1,36 @@
 import Card from "./Card";
 import Button from "./Button";
 import classes from "./Error.module.css";
+import reactDom from "react-dom";
 
+const Backdrop = backDropProps => {
+    return <div className={classes.backdrop} onClick={backDropProps.onClick} />
+}
+const ModelOverlay = modelProps => {
+    return (
+        <Card classname={classes.modal}>
+            <header className={classes.header}>
+            <h2>{modelProps.title}</h2>
+            </header>
+            <section className={classes.content}>
+            <p>{modelProps.message}</p>
+            </section>
+            <footer className={classes.actions}>
+            <Button onClick={modelProps.onClick} >Okay</Button>
+            </footer>
+        </Card>
+    )
+}
 const ErrorModel = (props) => {
   return (
     <>
-        <div className={classes.backdrop} onClick={props.onOkayButtonClick} />
-        <Card classname={classes.modal}>
-            <header className={classes.header}>
-            <h2>{props.title}</h2>
-            </header>
-            <section className={classes.content}>
-            <p>{props.message}</p>
-            </section>
-            <footer className={classes.actions}>
-            <Button onClick={props.onOkayButtonClick} >Okay</Button>
-            </footer>
-        </Card>
+    {reactDom.createPortal(<Backdrop onClick={props.onOkayButtonClick}/>, document.getElementById('backdrop-root'))}
+    {reactDom.createPortal(
+        <ModelOverlay
+            onClick={props.onOkayButtonClick}
+            title={props.title}
+            message={props.message}
+        />, document.getElementById('overlay-root'))}
     </>
   );
 };
